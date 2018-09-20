13b36ecadf89df4f1a89f914331029e94ce94f07
Malakai <41552723+Whomever0@users.noreply.github.com>
2018-09-20 14:11:13 +0100

    The configuration, basically creates the json that holds the prefix and token.

---
+++ Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CBot
+{
+    class Configuration
+    {
+        private const string configFolder = "Resources";
+        private const string configFile = "config.json";
+        public static BotConfiguration bot;
+
+        static Configuration(){
+            if(!Directory.Exists(configFolder))
+                Directory.CreateDirectory(configFolder);
+        
+            if(!File.Exists(configFolder + "/" + configFile)){
+                bot = new BotConfiguration();
+                string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+                File.WriteAllText(configFolder + "/" + configFile, json);
+            }else{
+                string json = File.ReadAllText(configFolder + "/" + configFile);
+                bot = JsonConvert.DeserializeObject<BotConfiguration>(json);
+            }
+        }
+    }
+
+    public struct BotConfiguration{
+        public string token;
+        public string prefix;
+        public string prefix2;
+    }
+}
